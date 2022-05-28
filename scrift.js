c11916529f2f66097e165df15194acc4413704d7
SM-Ashraf <106428314+SM-Ashraf@users.noreply.github.com>
2022-05-28 03:06:46 -0700

    Add files via upload

---
+++ scrift.js
@@ -0,0 +1,11 @@
+//Turn on Function 
+function bulbon(){
+    let bulbobj=document.querySelector(".bulboff");
+    bulbobj.setAttribute("src","images/turnon.gif");
+
+}
+function bulboff(){
+    let bulbobj=document.querySelector(".bulboff");
+    bulbobj.setAttribute("src","images/turnoff.png");
+
+}
